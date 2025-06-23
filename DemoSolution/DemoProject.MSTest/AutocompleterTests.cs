a7d08401f9c68de5619e5a991682fbac7041f147
JP <jp@jp.nl>
2025-06-23 13:58:07 +0200

    TDD laten zien

---
+++ DemoSolution/DemoProject.MSTest/AutocompleterTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.MSTest;
+
+[TestClass]
+public class AutocompleterTests
+{
+    List<Car> _cars = null!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _cars = new List<Car>
+        {
+            new() { Make = "Volvo", Model = "XC40", YearOfBuild = 2007 },
+            new() { Make = "Volvo", Model = "V40", YearOfBuild = 1976 },
+            new() { Make = "Peugeot", Model = "e208", YearOfBuild = 2018 },
+            new() { Make = "Mitsubishi", Model = "Outlander", YearOfBuild = 2007 },
+            new() { Make = "Kia", Model = "e-Niro", YearOfBuild = 2013 },
+            new() { Make = "Audi", Model = "E-tron", YearOfBuild = 2015 },
+            new() { Make = "Tesla", Model = "Model 3", YearOfBuild = 2016},
+            new() { Make = "Renault", Model = "Megane", YearOfBuild = 2009 },
+            new() { Make = "Renault", Model = "Twingo", YearOfBuild = 1998 },
+        };
+    }
+
+    [TestMethod]
+    public void Autocomplete_BasicQueryThatHandlesMultipleDataTypse_GiveSuggestions()
+    {
+        // Arrange
+        var sut = new Autocompleter<Car>
+        {
+            Query = "g",
+            Data = _cars
+        };
+
+        // Act
+        sut.Autocomplete();
+
+        // Assert
+        Assert.AreEqual(3, sut.Suggestions!.Count);
+    }
+
+    [TestMethod]
+    //[ExpectedException(typeof(ArgumentException))]
+    public void Autocomplete_NoData_Throws()
+    {
+        var sut = new Autocompleter<Car>
+        {
+            Query = "g",
+            Data = null!
+        };
+        var act = () => sut.Autocomplete();
+        Assert.ThrowsException<NotSupportedException>(act);
+    }
+
+    [TestMethod]
+    //[ExpectedException(typeof(ArgumentException))]
+    public void Autocomplete_NullInData_IgnoresNullAndGiveSuggestions()
+    {
+        _cars[2] = null!;
+        var sut = new Autocompleter<Car>
+        {
+            Query = "g",
+            Data = _cars
+        };
+        sut.Autocomplete();
+        Assert.AreEqual(2, sut.Suggestions!.Count);
+    }
+}
---
+++ DemoSolution/DemoProject/Autocompleter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject;
+
+public class Autocompleter<T>
+{
+    public required string Query { get; set; }
+    public required List<T> Data { get; set; }
+    public List<T>? Suggestions { get; set; }
+
+    public void Autocomplete()
+    {
+        if (Data is null)
+        {
+            throw new NotSupportedException("Data is null");
+        }
+
+        Suggestions = new();
+
+        foreach (var item in Data)
+        {
+            if (item is null)
+            {
+                continue;
+            }
+            
+            var props = item.GetType().GetProperties(); // make model
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(item) as string;
+                if (value is not null && value.Contains(Query))
+                {
+                    Suggestions.Add(item);
+                }
+            }
+        }
+    }
+}
